598f2de6037d798e5797cb2aa7312494e47c67a1
feridum <aleksander.patschek@gmail.com>
2018-12-31 18:24:31 +0100

    Updates

---
+++ precache-manifest.631a77db8c3b919944b18d8b399180e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a013d5cc0107198b3cb8",
+    "url": "/github-actions-validation-demo/static/css/main.6e214919.chunk.css"
+  },
+  {
+    "revision": "a013d5cc0107198b3cb8",
+    "url": "/github-actions-validation-demo/static/js/main.a013d5cc.chunk.js"
+  },
+  {
+    "revision": "11f389cbb760eeefccbc",
+    "url": "/github-actions-validation-demo/static/js/1.11f389cb.chunk.js"
+  },
+  {
+    "revision": "321f8b10a862326edcff",
+    "url": "/github-actions-validation-demo/static/js/runtime~main.321f8b10.js"
+  },
+  {
+    "revision": "37d78f867f8c99e54a0988f65c646adf",
+    "url": "/github-actions-validation-demo/index.html"
+  }
+];
